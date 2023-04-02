a4939918316a1bf256be265207baec7963517ac7
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-02 13:03:13 +0530

    Update counterContext.js

--- src/counterContext.js
+++ src/counterContext.js
@@ -1,23 +1,43 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-
-
-
-
-const counterContext = createContext()
-
+const counterContext = createContext();
 
 const CounterStateContext = (props) => {
+  const [count, setCount] = useState(0);
 
-    return (
-        <div id="counter-context">
-            <counterContext.Provider value={{ count, setCount }}>
-                {props.children}
-            </counterContext.Provider >
-        </div>
+  return (
+    <div id="counter-context">
+      <counterContext.Provider value={{ count, setCount }}>
+        {props.children}
+      </counterContext.Provider>
+    </div>
+  );
+};
 
-    )
-}
+export { counterContext, CounterStateContext };
 
-export { counterContext, CounterStateContext }
+
+
+// import React, { createContext } from "react";
+
+
+
+
+
+// const counterContext = createContext()
+
+
+// const CounterStateContext = (props) => {
+
+//     return (
+//         <div id="counter-context">
+//             <counterContext.Provider value={{ count, setCount }}>
+//                 {props.children}
+//             </counterContext.Provider >
+//         </div>
+
+//     )
+// }
+
+// export { counterContext, CounterStateContext }
 
